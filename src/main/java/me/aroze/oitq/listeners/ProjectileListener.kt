dd2a428dc47c8da803729ef78b27033c13b74f29
UwUAroze <admin@aroze.me>
2023-09-16 01:42:07 +0100

    fix removing arrows on land

--- src/main/java/me/aroze/oitq/listeners/ProjectileListener.kt
+++ src/main/java/me/aroze/oitq/listeners/ProjectileListener.kt
@@ -1,10 +1,12 @@
 package me.aroze.oitq.listeners
 
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.ProjectileHitEvent
 
 object ProjectileListener : Listener {
 
+    @EventHandler
     fun onProjectileLand(event: ProjectileHitEvent) {
         event.entity.remove()
     }
